b2ac5c2ada71ba4c4a60b8cf3a377d0b7f95e6c6
Shojan890 <johanpapoy@gmail.com>
2025-02-12 01:28:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "<https://voca.ro/1c4hwydWI3Dh", // Music streaming URL
+        musicUrl: "<https://audio.jukehost.co.uk/LHoOedqKlfpT0IHg6gafX9TWXd4iK5W8", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
