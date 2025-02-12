23fc145899def08ed162382f75d2835cede2d78a
Shojan890 <johanpapoy@gmail.com>
2025-02-12 01:10:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "<https://drive.google.com/file/d/1ZuXJklk61o3daNpbsB-mUhZfnUWWO6gM/view?usp=sharing", // Music streaming URL
+        musicUrl: "<https://voca.ro/1c4hwydWI3Dh", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
